459567452914144866f15baf15d2739eef8c34ef
sow-dev <sowz.dev@gmail.com>
2022-07-19 23:21:49 +0700

    config

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,23 @@
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
-import path from "path";
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://localhost:44305',
+        changeOrigin: true,
+        secure: false,
+        ws: true
+      }
+    }
+  },
   plugins: [vue()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-});
+      '@': path.resolve(__dirname, './src')
+    }
+  }
+})
