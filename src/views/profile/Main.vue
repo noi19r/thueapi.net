2118d60639983eca592c6b99c657ddaf8620f38a
sow-dev <sowz.dev@gmail.com>
2022-07-23 21:49:19 +0700

    update telegram

--- src/views/profile/Main.vue
+++ src/views/profile/Main.vue
@@ -81,83 +81,96 @@
         <div class="p-5">
           <div class="flex flex-col-reverse xl:flex-row flex-col">
             <div class="flex-1 mt-6 xl:mt-0">
-              <div class="grid grid-cols-12 gap-x-5">
-                <div class="col-span-12 2xl:col-span-6">
-                  <div>
-                    <label for="update-profile-form-1" class="form-label">Họ & Tên</label>
-                    <input
-                      id="update-profile-form-1"
-                      type="text"
-                      class="form-control"
-                      placeholder="Input text"
-                      :value="userInfo.name"
-                      disabled
-                    />
+              <form class="validate-form" @submit.prevent="saveTelegram">
+                <div class="grid grid-cols-12 gap-x-5">
+                  <div class="col-span-12 2xl:col-span-6">
+                    <div>
+                      <label for="update-profile-form-1" class="form-label">Họ & Tên</label>
+                      <input
+                        id="update-profile-form-1"
+                        type="text"
+                        class="form-control"
+                        placeholder="Input text"
+                        :value="userInfo.name"
+                        disabled
+                      />
+                    </div>
+                    <div class="mt-3">
+                      <label for="update-profile-form-6" class="form-label">Email</label>
+                      <input
+                        id="update-profile-form-6"
+                        type="text"
+                        class="form-control"
+                        placeholder="Input text"
+                        :value="userInfo.email"
+                        disabled
+                      />
+                    </div>
+                    <div class="mt-3">
+                      <label for="update-profile-form-10" class="form-label">Telegram</label>
+                      <input
+                        id="update-profile-form-9"
+                        v-model.trim="validateTelegram.username.$model"
+                        type="text"
+                        class="form-control"
+                        :class="{ 'border-danger': validateTelegram.username.$error }"
+                        placeholder="Input text"
+                      />
+
+                      <template v-if="validateTelegram.username.$error">
+                        <div
+                          v-for="(error, index) in validateTelegram.username.$errors"
+                          :key="index"
+                          class="text-danger mt-2"
+                        >
+                          {{ error.$message }}
+                        </div>
+                      </template>
+                    </div>
                   </div>
-                  <div class="mt-3">
-                    <label for="update-profile-form-6" class="form-label">Email</label>
-                    <input
-                      id="update-profile-form-6"
-                      type="text"
-                      class="form-control"
-                      placeholder="Input text"
-                      :value="userInfo.email"
-                      disabled
-                    />
-                  </div>
-                  <div class="mt-3">
-                    <label for="update-profile-form-10" class="form-label">Telegram</label>
-                    <input
-                      id="update-profile-form-9"
-                      type="text"
-                      class="form-control"
-                      placeholder="Input text"
-                      :value="userInfo.telegram.username"
-                    />
+                  <div class="col-span-12 2xl:col-span-6">
+                    <div class="mt-3 2xl:mt-0">
+                      <label for="update-profile-form-3" class="form-label">Số Dư Hiện Tại</label>
+                      <input
+                        id="update-profile-form-3"
+                        type="text"
+                        class="form-control"
+                        placeholder="Input text"
+                        :value="`${$h.formatCurrency(userInfo.amount)} vnđ`"
+                        disabled
+                      />
+                    </div>
+                    <div class="mt-3">
+                      <label for="update-profile-form-4" class="form-label">Số Điện Thoại</label>
+                      <input
+                        id="update-profile-form-4"
+                        type="text"
+                        class="form-control"
+                        placeholder="Input text"
+                        :value="userInfo.phone"
+                        disabled
+                      />
+                    </div>
+                    <div class="mt-3">
+                      <label for="update-profile-form-9" class="form-label">ID Telegram</label>
+                      <input
+                        id="update-profile-form-9"
+                        type="text"
+                        class="form-control"
+                        placeholder=""
+                        :value="userInfo.telegram.id"
+                        disabled
+                      />
+                    </div>
                   </div>
                 </div>
-                <div class="col-span-12 2xl:col-span-6">
-                  <div class="mt-3 2xl:mt-0">
-                    <label for="update-profile-form-3" class="form-label">Số Dư Hiện Tại</label>
-                    <input
-                      id="update-profile-form-3"
-                      type="text"
-                      class="form-control"
-                      placeholder="Input text"
-                      :value="`${$h.formatCurrency(userInfo.amount * 10000)} vnđ`"
-                      disabled
-                    />
-                  </div>
-                  <div class="mt-3">
-                    <label for="update-profile-form-4" class="form-label">Số Điện Thoại</label>
-                    <input
-                      id="update-profile-form-4"
-                      type="text"
-                      class="form-control"
-                      placeholder="Input text"
-                      :value="userInfo.phone"
-                      disabled
-                    />
-                  </div>
-                  <div class="mt-3">
-                    <label for="update-profile-form-9" class="form-label">ID Telegram</label>
-                    <input
-                      id="update-profile-form-9"
-                      type="text"
-                      class="form-control"
-                      placeholder="Input text"
-                      :value="userInfo.telegram.id"
-                      disabled
-                    />
-                  </div>
+                <div class="flex justify-end mt-4">
+                  <button class="btn btn-primary w-25 mr-auto">Lưu thông tin</button>
+                  <a href="" class="text-danger flex items-center">
+                    <Trash2Icon class="w-4 h-4 mr-1" /> Delete Account
+                  </a>
                 </div>
-              </div>
-              <div class="flex justify-end mt-4">
-                <button type="button" class="btn btn-primary w-25 mr-auto">Lưu thông tin</button>
-                <a href="" class="text-danger flex items-center">
-                  <Trash2Icon class="w-4 h-4 mr-1" /> Delete Account
-                </a>
-              </div>
+              </form>
             </div>
           </div>
         </div>
@@ -170,20 +183,24 @@
           <h2 class="font-medium text-base mr-auto">Đổi Mật Khẩu</h2>
         </div>
         <div class="p-5">
-          <form class="validate-form" @submit.prevent="save">
+          <form class="validate-form" @submit.prevent="saveChangePassword">
             <div>
               <label for="change-password-form-1" class="form-label">Mật khẩu cũ</label>
               <input
                 id="change-password-form-1"
-                v-model.trim="validate.currentPassword.$model"
+                v-model.trim="validateChangePassword.currentPassword.$model"
                 type="password"
                 name="password"
                 class="form-control"
-                :class="{ 'border-danger': validate.currentPassword.$error }"
+                :class="{ 'border-danger': validateChangePassword.currentPassword.$error }"
                 placeholder="Nhập mật khẩu cũ"
               />
-              <template v-if="validate.currentPassword.$error">
-                <div v-for="(error, index) in validate.currentPassword.$errors" :key="index" class="text-danger mt-2">
+              <template v-if="validateChangePassword.currentPassword.$error">
+                <div
+                  v-for="(error, index) in validateChangePassword.currentPassword.$errors"
+                  :key="index"
+                  class="text-danger mt-2"
+                >
                   {{ error.$message }}
                 </div>
               </template>
@@ -192,15 +209,19 @@
               <label for="change-password-form-2" class="form-label">Mật khẩu mới</label>
               <input
                 id="change-password-form-2"
-                v-model.trim="validate.password.$model"
+                v-model.trim="validateChangePassword.password.$model"
                 type="password"
                 name="password"
                 class="form-control"
-                :class="{ 'border-danger': validate.password.$error }"
+                :class="{ 'border-danger': validateChangePassword.password.$error }"
                 placeholder="Nhập mật khẩu mới"
               />
-              <template v-if="validate.password.$error">
-                <div v-for="(error, index) in validate.password.$errors" :key="index" class="text-danger mt-2">
+              <template v-if="validateChangePassword.password.$error">
+                <div
+                  v-for="(error, index) in validateChangePassword.password.$errors"
+                  :key="index"
+                  class="text-danger mt-2"
+                >
                   {{ error.$message }}
                 </div>
               </template>
@@ -209,15 +230,19 @@
               <label for="change-password-form-3" class="form-label">Nhập lại mật khẩu</label>
               <input
                 id="change-password-form-3"
-                v-model.trim="validate.surePassword.$model"
+                v-model.trim="validateChangePassword.surePassword.$model"
                 type="password"
                 name="password"
                 class="form-control"
-                :class="{ 'border-danger': validate.surePassword.$error }"
+                :class="{ 'border-danger': validateChangePassword.surePassword.$error }"
                 placeholder="Nhập lại mật khẩu mới"
               />
-              <template v-if="validate.surePassword.$error">
-                <div v-for="(error, index) in validate.surePassword.$errors" :key="index" class="text-danger mt-2">
+              <template v-if="validateChangePassword.surePassword.$error">
+                <div
+                  v-for="(error, index) in validateChangePassword.surePassword.$errors"
+                  :key="index"
+                  class="text-danger mt-2"
+                >
                   {{ error.$message }}
                 </div>
               </template>
@@ -328,27 +353,7 @@
         <div class="flex items-center p-5 border-b border-slate-200/60 dark:border-darkmode-400">
           <h2 class="font-medium text-base mr-auto">Bảo mật 2 lớp - 2FA</h2>
         </div>
-        <!-- <div class="box">
-          <div class="p-5">
-            <div
-              class="h-40 2xl:h-56 image-fit rounded-md overflow-hidden before:block before:absolute before:w-full before:h-full before:top-0 before:left-0 before:z-10 before:bg-gradient-to-t before:from-black before:to-black/10"
-            >
-              <img
-                alt="Midone - HTML Admin Template"
-                class="rounded-md"
-                :src="'https://api.vietqr.io/970416/222629219/0/NAP3091/970416-222629219-SUdZpzb.jpg?accountName=Nguy%E1%BB%85n%20Tr%E1%BB%8Dng%20V%E1%BB%B9'"
-              />
-            </div>
-            <div class="text-slate-600 dark:text-slate-500 mt-5">
-              <div class="flex items-center"><LinkIcon class="w-4 h-4 mr-2" /> Price: $12</div>
-              <div class="flex items-center mt-2"><LayersIcon class="w-4 h-4 mr-2" /> Remaining Stock: 123123</div>
-              <div class="flex items-center mt-2">
-                <CheckSquareIcon class="w-4 h-4 mr-2" />
-                Status: {{ true ? 'Active' : 'Inactive' }}
-              </div>
-            </div>
-          </div>
-        </div> -->
+
         <div class="p-5">
           <div class="grid grid-cols-12 gap-x-5">
             <div class="col-span-12 xl:col-span-6" v-if="!userInfo.is2FA">
@@ -362,16 +367,31 @@
                 'xl:col-span-6': !userInfo.is2FA
               }"
             >
-              <div class="mt-3 xl:mt-0">
-                <label for="update-profile-form-10" class="form-label">Xác nhận OTP</label>
-                <input id="update-profile-form-10" type="text" class="form-control" placeholder="OTP" value="" />
-              </div>
+              <form class="validate-form" @submit.prevent="save2FA">
+                <div class="mt-3 xl:mt-0">
+                  <label for="update-profile-form-10" class="form-label">Xác nhận OTP</label>
+                  <input
+                    id="2fa-form-3"
+                    v-model.trim="validate2FA.otp.$model"
+                    class="form-control"
+                    :class="{ 'border-danger': validate2FA.otp.$error }"
+                    type="text"
+                    name="otp"
+                    placeholder="OTP"
+                  />
+                  <template v-if="validate2FA.otp.$error">
+                    <div v-for="(error, index) in validate2FA.otp.$errors" :key="index" class="text-danger mt-2">
+                      {{ error.$message }}
+                    </div>
+                  </template>
+                </div>
 
-              <div class="flex justify-end mt-4">
-                <button type="button" class="btn btn-primary w-20 mr-auto">
-                  {{ !userInfo.is2FA ? 'Bật 2FA' : 'Tắt 2FA' }}
-                </button>
-              </div>
+                <div class="flex justify-end mt-4">
+                  <button class="btn btn-primary w-20 mr-auto">
+                    {{ !userInfo.is2FA ? 'Bật 2FA' : 'Tắt 2FA' }}
+                  </button>
+                </div>
+              </form>
             </div>
           </div>
         </div>
@@ -389,11 +409,10 @@ import { useVuelidate } from '@vuelidate/core'
 
 import { useUserStore } from '@/stores/user'
 import { helper as $h } from '@/utils/helper'
-import { qr2FA, changePassword } from '@/api'
+import { qr2FA, changePassword, verify2FA, updateTelegram } from '@/api'
 import { toast } from '../../plugins/toast'
 
 const userStore = useUserStore()
-userStore.setUserInfo()
 
 const userInfo = computed(() => userStore.userInfoMe)
 const select = ref('1')
@@ -401,11 +420,24 @@ const activeTab = ref('info')
 const qrcode = ref('')
 const deleteConfirmationModal = ref(false)
 
-const formData = reactive({
+const formTelegramData = reactive({
+  username: userStore.userInfoMe.telegram.username
+})
+
+const formChangePasswordData = reactive({
   currentPassword: '',
   password: '',
   surePassword: ''
 })
+const rulesTelegram = computed(() => {
+  return {
+    username: {
+      required,
+      minLength: minLength(3)
+    }
+  }
+})
+const validateTelegram = useVuelidate(rulesTelegram, toRefs(formTelegramData))
 
 const rules = computed(() => {
   return {
@@ -419,19 +451,31 @@ const rules = computed(() => {
     },
     surePassword: {
       required,
-      sameAs: sameAs(formData.password)
+      sameAs: sameAs(formChangePasswordData.password)
+    }
+  }
+})
+const form2FAData = reactive({
+  otp: ''
+})
+const rules2FA = computed(() => {
+  return {
+    otp: {
+      required,
+      minLength: minLength(6)
     }
   }
 })
 
-const validate = useVuelidate(rules, toRefs(formData))
+const validateChangePassword = useVuelidate(rules, toRefs(formChangePasswordData))
+const validate2FA = useVuelidate(rules2FA, toRefs(form2FAData))
 
-const save = async () => {
-  validate.value.$touch()
-  if (validate.value.$invalid) {
+const saveChangePassword = async () => {
+  validateChangePassword.value.$touch()
+  if (validateChangePassword.value.$invalid) {
     toast.danger('Vui lòng kiểm tra lại thông tin')
   } else {
-    let data = toRaw(formData)
+    let data = toRaw(formChangePasswordData)
     await changePassword(data)
 
     toast.success('Thay đổi mật khẩu thành công')
@@ -439,10 +483,38 @@ const save = async () => {
   }
 }
 
+const save2FA = async () => {
+  validate2FA.value.$touch()
+  if (validate2FA.value.$invalid) {
+    toast.danger('Vui lòng kiểm tra lại OTP đã nhập')
+  } else {
+    let data = toRaw(form2FAData)
+    await verify2FA(data)
+    await userStore.setUserInfo()
+    toast.success('Thay xác thực 2 yếu tố thành công')
+    if (!userStore.userInfoMe.is2FA) {
+      let qrImage = await qr2FA()
+      qrcode.value = qrImage.image
+    }
+  }
+}
+
+const saveTelegram = async () => {
+  validateTelegram.value.$touch()
+  if (validateTelegram.value.$invalid) {
+    toast.danger('Vui lòng kiểm tra lại thông tin')
+  } else {
+    let data = toRaw(formTelegramData)
+    await updateTelegram(data)
+    userStore.setUserInfo()
+    toast.success('Cập nhật thông tin thành công')
+  }
+}
+
 onMounted(async () => {
+  userStore.setUserInfo()
   if (!userStore.userInfoMe.is2FA) {
     let qrImage = await qr2FA()
-
     qrcode.value = qrImage.image
   }
 })
