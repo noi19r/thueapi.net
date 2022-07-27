5113a02cef89f05086ed6d06f1ec2231b45c5fb0
sow-dev <sowz.dev@gmail.com>
2022-07-27 23:08:31 +0700

    fix

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
-import obfuscator from 'rollup-plugin-obfuscator'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   server: {
     proxy: {
@@ -17,35 +15,8 @@ export default defineConfig({
       }
     }
   },
-  build: {
-    outDir: 'build'
-  },
 
-  plugins: [
-    vue(),
-    {
-      ...obfuscator({
-        fileOptions: {
-          compact: true,
-          controlFlowFlattening: true,
-          controlFlowFlatteningThreshold: 0.5,
-          identifierNamesGenerator: 'hexadecimal',
-          rotateStringArray: true,
-          shuffleStringArray: true,
-          splitStrings: true,
-          splitStringsChunkLength: 4,
-          stringArray: true,
-          stringArrayEncoding: ['base64'],
-          stringArrayThreshold: 1,
-          stringArrayWrappersCount: 2,
-          transformObjectKeys: true
-        },
-        include: ['**/*.js', '**/*.jsx']
-      }),
-      apply: 'build',
-      enforce: 'post'
-    }
-  ],
+  plugins: [vue()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src')
