aa2310872607c88830112c0905f42aae170fc940
sow-dev <sowz.dev@gmail.com>
2022-07-27 23:07:58 +0700

    fix error time out axios

--- src/axios/index.js
+++ src/axios/index.js
@@ -34,12 +34,17 @@ function requestService(config) {
       return res.data.data
     },
     (error) => {
+      if (error.code == 'ECONNABORTED') {
+        toast.danger('Thất bại, vui lòng thử lại sau ít phút')
+        return Promise.reject(error.message)
+      }
       if (error.response.status == 401) {
         localStorage.removeItem('token')
         sessionStorage.removeItem('token')
         localStorage.removeItem('userInfo')
         sessionStorage.removeItem('userInfo')
         router.replace({ name: 'login' })
+        toast.danger('Phiên bản đang nhập đã hết hạn, vui lòng đăng nhập lại.')
       } else {
         toast.danger(error.response.data.error.message)
       }
